c0be4780bddc8e9723eb1d2148275fcd9ccfd06c
Rasmus <rasmus.raasuke@gmail.com>
2026-04-21 18:23:49 +0300

    show logo on Header

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -7,13 +7,16 @@
 <header class="bg-stone-50 border-b px-4 py-3 flex flex-wrap gap-3 justify-between items-center">
   <div class="flex gap-2.5 items-center">
     {#if title}
-      <h1 class="font-bold text-2xl mr-4">{title}</h1>
+      <div class="flex gap-2 items-center">
+        <img src="/public/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
+        <h1 class="font-bold text-2xl">{title}</h1>
+      </div>
     {/if}
   </div>
   <div>
     <a href="/oauth?redirect=/" class="link-button gap-2">
       {t.login}
-      <img src="/public/img/google.svg" class="h-4 w-4" title="Google Login" alt="">
+      <img src="/public/img/google.svg" class="size-4" title="Google Login" alt="">
     </a>
   </div>
 </header>
