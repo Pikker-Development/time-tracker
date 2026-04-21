e21d8e0f699aaa6bf5247fd210fd25bdfeb83a1a
Rasmus <rasmus.raasuke@gmail.com>
2026-04-21 19:04:51 +0300

    homepage is still loading its style :P

--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import MainPageLayout from 'src/layout/MainPageLayout.svelte'
+  import Spinner from 'src/components/Spinner.svelte'
 </script>
 
-<MainPageLayout>
+<MainPageLayout class="relative">
+  <Spinner class="absolute inset-0"/>
 </MainPageLayout>
