2199e93f171d2325ca70b172478bd7b97266452d
Rasmus <rasmus.raasuke@gmail.com>
2026-04-21 18:59:51 +0300

    match header x padding with content's x padding

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -2,7 +2,7 @@
   import {t} from 'i18n'
 </script>
 
-<header class="bg-stone-50 border-b px-4 py-3 flex flex-wrap gap-3 justify-between items-center">
+<header class="bg-stone-50 border-b px-2 sm:px-3 py-3 flex flex-wrap gap-3 justify-between items-center">
   <div class="flex gap-2.5 items-center">
     <div class="flex gap-2 items-center">
       <img src="/public/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
