b4e443b9ab3fdaeb159bc369cf6febe3899e9298
KRSssssss <krisrobin.metsoja@gmail.com>
2026-04-23 12:29:02 +0300

    #200799543 home page layout changed

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -6,12 +6,14 @@
 </script>
 
 <header class="bg-stone-50 border-b px-2 sm:px-3 py-3 flex flex-wrap gap-3 justify-between items-center">
-  <div class="flex gap-2.5 items-center">
-    <div class="flex gap-2 items-center">
-      <img src="/public/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
-      <h1 class="font-bold text-2xl">{t.title}</h1>
+  <a href="/">
+    <div class="flex gap-2.5 items-center">
+      <div class="flex gap-2 items-center">
+        <img src="/public/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
+        <h1 class="font-bold text-2xl">{t.title}</h1>
+      </div>
     </div>
-  </div>
+  </a>
   <Navigation/>
   <Avatar />
 </header>
