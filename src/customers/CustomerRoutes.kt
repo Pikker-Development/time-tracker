d7f74891c3a8f1adf2d2c8a1560ce28dd3d1d99b
KRSssssss <krisrobin.metsoja@gmail.com>
2026-04-23 11:28:12 +0300

    #200771547 Customer listing route

--- src/customers/CustomerRoutes.kt
+++ src/customers/CustomerRoutes.kt
@@ -1,10 +1,12 @@
 package customers
 
 import auth.Access
+import klite.annotations.GET
 import klite.annotations.POST
 import users.Role.ADMIN
 
 @Access(ADMIN)
 class CustomerRoutes(private val repository: CustomerRepository) {
+  @GET fun list() = repository.list()
   @POST fun create(customer: Customer) = repository.save(customer)
 }
