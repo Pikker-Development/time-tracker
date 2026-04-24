74d12469d2d0549a1125325ac086dc94df30fd60
liisakanemagi <liisa.kanemagi@gmail.com>
2026-04-24 16:31:52 +0300

    #200799551 show customer name instead of id on project page and add an edit button

--- ui/src/pages/projects/ProjectPage.svelte
+++ ui/src/pages/projects/ProjectPage.svelte
@@ -1,28 +1,33 @@
 <script lang="ts">
 
 import MainPageLayout from 'src/layout/MainPageLayout.svelte'
-import type {Id, Project} from 'src/api/types'
+import type {Customer, Id, Project} from 'src/api/types'
 import {onMount} from 'svelte'
 import api from 'src/api/api'
 import {t} from 'i18n'
+import NewProjectButton from 'src/pages/projects/NewProjectButton.svelte'
 
 export let id: Id<Project>
 
 let project: Project
+let customerMap: Record<string, string> = {}
 
 onMount(async () => {
   project = await api.get('projects/' + id)
+  const customerList: Customer[] = await api.get('customers')
+  customerMap = Object.fromEntries(customerList.map(c => [c.id, c.name]))
 })
-
 </script>
 
 <MainPageLayout class="relative">
   {#if project}
+    <div class="flex justify-end"><NewProjectButton {project} label={t.projects.edit}/></div>
+
     <h2 class="text-2xl font-bold mb-4">{project?.name}</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <div>
         <p class="text-sm text-gray-500">{t.customers.customer}</p>
-        <p class="text-lg font-medium">{project.customerId}</p>
+        <p class="text-lg font-medium">{customerMap[project.customerId]}</p>
       </div>
       <div>
         <p class="text-sm text-gray-500">{t.projects.description}</p>
