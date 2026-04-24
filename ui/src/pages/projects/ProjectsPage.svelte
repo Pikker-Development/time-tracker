90c3388961daae3317ed7fc45119faa45d18fd5e
liisakanemagi <liisa.kanemagi@gmail.com>
2026-04-24 11:04:03 +0300

    #200799548 User can see customer name and Story Tracker id on project card.

--- ui/src/pages/projects/ProjectsPage.svelte
+++ ui/src/pages/projects/ProjectsPage.svelte
@@ -7,21 +7,28 @@ import api from 'src/api/api'
 import {t} from 'i18n'
 
 let projects: Project[]
+let customerMap: Record<string, string> = {}
 
 onMount(async () => {
   projects = await api.get('projects')
+  const customerList: Customer[] = await api.get('customers')
+  customerMap = Object.fromEntries(customerList.map(c => [c.id, c.name]))
 })
 </script>
 
 <MainPageLayout class="relative">
   <NewProjectButton/>
-
   <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-6 my-3 text-lg">
     {#each projects ?? [] as p}
       <div class="project border rounded-lg px-4 py-3 bg-white hover:bg-stone-50">
         <div class="font-semibold">{p.name}</div>
+        <div class="flex justify-between text-muted text-sm">
+          <div>{t.customers.customer} {customerMap[p.customerId]}</div>
+          {#if p.storyTrackerId}
+            <div>{t.projects.storyTrackerId} {p.storyTrackerId}</div>
+          {/if}
+        </div>
       </div>
-      {/each}
+    {/each}
   </div>
-
 </MainPageLayout>
